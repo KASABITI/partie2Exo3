323a1cea56b1609727c84620e8fecf80b4bb1407
KASABITI <aggieksbt@gmail.com>
2021-06-20 08:27:34 +0200

    First commit exo3 partie2

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Partie 2 exo 3</title>
+</head>
+<body>
+    <?php
+    // Créez une variable initialisé à 1. Tant que cette variable n'a pas atteint 15, affichez 'On y est presque'.
+    
+    // echo "<h3 style='color:red;'>exercice 3: </h3><br>";
+    // for($i=1;$i<=15;$i++){
+    //     if($i != 15){
+    //         echo "On y presque ";
+    //     }else{
+    //         echo "on a atteint ";
+    //     }
+    //    echo $i."<br>";
+
+    // }
+     
+    for ($b=1;$b<=100;$b += 15){
+        var_dump($b);
+
+        // if ($z < 100){
+        //   echo "ma variable est = ".$b." je lui rajoute '15' donc maintenant ma variable est =" .$z."<br>";
+            
+        // }else {
+        //      return  "on a atteint 100";
+        // }
+        
+    }
+    
+    ?>
+</body>
+</html>
+
+
+<!-- ; $i=1
+    while($i <= 15) {
+      echo "<p>Tour $x : On y est presque</p>";
+      $i++;
+    } echo "<p>Voilà fini</p>"; -->
